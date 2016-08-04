506f35fbdebe9a639a95c29e74b360b985bd2da4
Prashant Tiwari <prashaantt@gmail.com>
2016-08-04 22:39:40 +0530

    Add serializer named export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,5 @@ import * as Models from "./models";
 export { Models };
 import * as Constants from "./constants";
 export { Constants };
+import * as Serializers from "./serializers";
+export { Serializers };
