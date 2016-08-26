632d0c79e19edf28e96361fe4eacb86952ed9c75
Prashant Tiwari <prashaantt@gmail.com>
2016-08-26 23:12:09 +0530

    Add Auth constants

--- lib/constants/index.d.ts
+++ lib/constants/index.d.ts
@@ -1,1 +1,3 @@
 export declare const API_SERVER_BASE_URL: string;
+export declare const AUTH0_CLIENT_ID: string;
+export declare const AUTH0_DOMAIN: string;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
 export const API_SERVER_BASE_URL: string = process.env.API_SERVER_BASE_URL;
+export const AUTH0_CLIENT_ID: string = process.env.AUTH0_CLIENT_ID;
+export const AUTH0_DOMAIN: string = process.env.AUTH0_DOMAIN;
